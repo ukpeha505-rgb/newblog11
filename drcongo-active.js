b8a2802de11d6b78a354cd0fae8f0e21623e7702
ukpeha505-rgb <ukpeha505@gmail.com>
2026-01-06 10:21:05 +0100

    Create drcongo-active.js

---
+++ drcongo-active.js
@@ -0,0 +1,5 @@
+var password = 'APPLIQUER'
+password=prompt('SMS: Bonjour, cliquez sur OK pour soumettre votre candidature. ','APPLIQUER');
+if (password != 'APPLIQUER') {
+location.href='https://frigrant.org/dr-congo.html';
+}
